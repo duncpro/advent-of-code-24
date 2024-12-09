a68650e0f45007d8cac10372fc46a979fc675f73
Duncan <duncpro@icloud.com>
2024-12-09 01:41:09 -0500

    solve d7p2

---
+++ d7p2/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let mut sum_accepted_targets = 0u64; 
+
+    for line in input.lines() {
+        let [target_str, eq_str] = line.split(": ")
+            .collect::<Vec<&str>>().try_into().unwrap();
+
+        let target = target_str.parse::<u64>().unwrap();
+
+        let terms = eq_str.split(" ").map(|s| s.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+
+        let accepted = test(&terms[1..], terms[0], target);
+
+        if accepted {
+            sum_accepted_targets = sum_accepted_targets.checked_add(target)
+                .unwrap();
+        }
+    }
+
+    println!("{sum_accepted_targets}");
+}
+
+fn test(terms: &[u64], curr: u64, target: u64) -> bool {
+    if terms.is_empty() { return curr == target; }
+
+    let term = terms[0];
+    let rest = &terms[1..];
+
+    let mut accept = false;
+    accept |= test(rest, curr.checked_add(term).unwrap(), target);
+    accept |= test(rest, curr.checked_mul(term).unwrap(), target);
+    accept |= test(rest, concat_nums(curr, term), target);
+    return accept;
+}
+
+fn concat_nums(left: u64, right: u64) -> u64 {
+    let s = format!("{}{}", left, right);
+    return s.parse::<u64>().unwrap();
+}
